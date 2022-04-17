87400c509db6c587f256a475a399068f5468e1e2
Yevhen Zahorniak <zzagornyak@gmail.com>
2022-04-17 10:04:44 +0200

    add swiper section

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.slider-button__next',
+      prevEl: '.slider-button__prev',
+    },
+  });
