47c7fd8160d13135bb259c6202e71ae5dfaa0112
Yevhen Zahorniak <zzagornyak@gmail.com>
2022-04-22 11:41:00 +0200

    add reviews slider

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
-const swiper = new Swiper('.swiper', {
+const hotelSwiper = new Swiper('.hotel-slider', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button__next',
-      prevEl: '.slider-button__prev',
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+  const reviewsSwiper = new Swiper('.reviews-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
     },
   });
